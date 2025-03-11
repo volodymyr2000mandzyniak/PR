0d04227591a4dd6c9e33fe9d88ff2318424c54fe
volodymyr2000mandzyniak <volodimirmandzyniak@gmail.com>
2025-03-11 20:24:49 +0200

    last fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,70 +1,9 @@
-# class ProjectsController < ApplicationController
-#   before_action :authenticate_user!
-#   before_action :set_project, only: [:show, :update, :destroy]
-
-#   # GET /projects
-#   def index
-#     cache_key = "projects/#{Project.maximum(:updated_at)}"
-    
-#     @projects = Rails.cache.fetch(cache_key, expires_in: 12.hours) do
-#       Project.preload(:tasks)
-#             .select(:id, :name, :description, :created_at, :updated_at)
-#             .order(created_at: :desc).to_a
-#     end
-    
-#     render json: @projects, each_serializer: ProjectSerializer
-#   end
-
-#   # GET /projects/:id
-#   def show
-#     render json: @project, serializer: ProjectSerializer
-#   end
-
-#   # POST /projects
-#   def create
-#     @project = Project.new(project_params)
-#     if @project.save
-#       render json: @project, serializer: ProjectSerializer, status: :created
-#     else
-#       render json: @project.errors, status: :unprocessable_entity
-#     end
-#   end
-
-#   # PATCH/PUT /projects/:id
-#   def update
-#     if @project.update(project_params)
-#       render json: @project, serializer: ProjectSerializer
-#     else
-#       render json: @project.errors, status: :unprocessable_entity
-#     end
-#   end
-
-#   # DELETE /projects/:id
-#   def destroy
-#     @project.destroy
-#     head :no_content
-#   end
-
-#   private
-
-#   def set_project
-#     @project = Project.find(params[:id])
-#   end
-
-#   def project_params
-#     params.require(:project).permit(:name, :description)
-#   end
-# end
-
-
-
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_project, only: [:show, :update, :destroy]
 
   # GET /projects
   def index
-    # Виводимо лише проекти поточного користувача
     cache_key = "projects/#{current_user.id}/#{Project.maximum(:updated_at)}"
     
     @projects = Rails.cache.fetch(cache_key, expires_in: 12.hours) do
@@ -78,7 +17,6 @@ class ProjectsController < ApplicationController
 
   # GET /projects/:id
   def show
-    # Перевіряємо, чи проект належить поточному користувачу
     if @project.user_id == current_user.id
       render json: @project, serializer: ProjectSerializer
     else
@@ -88,7 +26,6 @@ class ProjectsController < ApplicationController
 
   # POST /projects
   def create
-    # Додаємо user_id до параметрів проекту
     @project = current_user.projects.new(project_params)
     if @project.save
       render json: @project, serializer: ProjectSerializer, status: :created
@@ -99,7 +36,6 @@ class ProjectsController < ApplicationController
 
   # PATCH/PUT /projects/:id
   def update
-    # Перевіряємо, чи проект належить поточному користувачу
     if @project.user_id == current_user.id
       if @project.update(project_params)
         render json: @project, serializer: ProjectSerializer
@@ -113,7 +49,6 @@ class ProjectsController < ApplicationController
 
   # DELETE /projects/:id
   def destroy
-    # Перевіряємо, чи проект належить поточному користувачу
     if @project.user_id == current_user.id
       @project.destroy
       head :no_content
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,42 @@
 # db/seeds.rb
+
+require 'faker'
+
+# Destroy existing data to start fresh
 Task.destroy_all
 Project.destroy_all
+User.destroy_all
 
-5.times do |i|
-  project = Project.create!(
-    name: "Проект #{i + 1}: #{Faker::App.name}",
-    description: Faker::Lorem.paragraph(sentence_count: 4),
-    created_at: Time.now - rand(1..30).days
+# Create multiple users with Faker data
+5.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: 'password123',
+    password_confirmation: 'password123'
   )
 
-  rand(3..5).times do |j|
-    Task.create!(
-      project: project,
-      name: "Завдання #{j + 1}: #{Faker::Lorem.sentence(word_count: 3)}",
-      description: Faker::Lorem.paragraph(sentence_count: 2),
-      status: Task.statuses.keys.sample,
-      created_at: project.created_at + rand(1..24).hours
+  # Create projects and tasks for each user
+  3.times do |i|
+    project = Project.create!(
+      name: "Проект #{i + 1}: #{Faker::App.name}",
+      description: Faker::Lorem.paragraph(sentence_count: 4),
+      created_at: Faker::Date.between(from: 30.days.ago, to: Date.today),
+      user: user
     )
+
+    rand(3..5).times do |j|
+      Task.create!(
+        project: project,
+        name: "Завдання #{j + 1}: #{Faker::Lorem.sentence(word_count: 3)}",
+        description: Faker::Lorem.paragraph(sentence_count: 2),
+        status: %w[new in_progress completed].sample,
+        created_at: Faker::Date.between(from: project.created_at, to: Date.today)
+      )
+    end
   end
 end
 
-puts "Створено:"
-puts "- #{Project.count} проектів"
-puts "- #{Task.count} завдань"
+puts "Created:"
+puts "- #{User.count} Users"
+puts "- #{Project.count} Projects"
+puts "- #{Task.count} Tasks"
